b72503b08422773c4a95c8e3f023f4d8b129e223
cousinbigcountry <fountainkylec.gmail.com>
2025-01-30 06:07:41 -0500

    added all drivers and team owners from 1949 first race

--- NASCAR-History.sql
+++ NASCAR-History.sql
@@ -1,27 +1,11 @@
-CREATE TABLE people (
-	personID SERIAL PRIMARY KEY,
-	nameFirst VARCHAR(255) NOT NULL,
-	nameLast VARCHAR(255) NOT NULL,
-	nameMiddle VARCHAR(255),
-	birthYear SMALLINT NOT NULL,
-	birthMonth SMALLINT NOT NULL,
-	birthDay SMALLINT NOT NULL,
-	birthCountry VARCHAR(255) NOT NULL,
-	birthState VARCHAR(255),
-	birthCity VARCHAR(255),
-	deathYear SMALLINT,
-	deathMonth SMALLINT,
-	deathDay SMALLINT,
-	birthDate DATE NOT NULL DEFAULT CURRENT_DATE,
-	deathDate DATE
-);
-
-ALTER TABLE people
-RENAME COLUMN birthcountry TO homecountry;
-
 SELECT * FROM people;
 
-INSERT INTO people (nameFirst, nameLast, nameMiddle, birthyear, birthMonth, birthDay, homeCountry, homeState, homeCity, deathYear, deathMonth, deathDay, birthDate, deathDate)
-VALUES('Buck', 'Baker', NULL, 1919, 03, 04, 'United States of America', 'North Carolina', 'Charlotte', 2002, 04, 14, '1919-03-04', '2002-04-14');
+ALTER TABLE people 
+ADD nickName VARCHAR(100);
 
-SELECT * FROM people;
+UPDATE people
+SET birthdate = '1914-06-05'
+WHERE personID = 42;
+
+INSERT INTO people (nameFirst, nameLast, nameMiddle, birthyear, birthMonth, birthDay, homeCountry, homeState, homeCity, deathYear, deathMonth, deathDay, birthDate, deathDate, status, gender)
+VALUES('Penny', 'Parks', NULL, 1914, 06, 05, 'United States of America', 'Georgia', 'Dawsonville', 2010, 06, 20, '1914-06-06', '2010-06-20', 'Retired', 'M');
